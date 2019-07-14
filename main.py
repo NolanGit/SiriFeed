6035daa11d1e6fcd93c7bd3b49a3f5fdbd4ca47c
NolanSun <shr1213@live.com>
2019-07-14 23:14:22 +0800

    20190714

--- main.py
+++ main.py
@@ -26,6 +26,7 @@ weather = Weather()
 today_tmp_max, today_tmp_min = weather.get_temp(key, location)
 
 aqi_json = weather.get_aqi(key, location)
-city_aqi = aqi_json['air_now_city']
-city_air_condition = city_aqi['qlty']
+city_air = aqi_json['air_now_city']
+city_air_condition = city_air['qlty']
+city_aqi = city_air['aqi']
 print('你现在在' + location + '，最高气温' + str(today_tmp_max) + '度，' + '最低气温' + str(today_tmp_min) + '度，' + '空气质量' + str(city_air_condition) + '，AQI' + str(city_air_condition))
