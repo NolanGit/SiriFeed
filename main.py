a11af240bfc1e91bd5d0a2c95f44f3e732eba457
NolanSun <shr1213@live.com>
2019-07-14 23:44:20 +0800

    20190714

--- main.py
+++ main.py
@@ -29,6 +29,7 @@ city_air = aqi_json['air_now_city']
 city_air_condition = city_air['qlty']
 city_aqi = city_air['aqi']
 comfort, clothes = weather.get_lifestyle(key, location)
-weather_content = '你现在在' + location + '，最高气温' + str(today_tmp_max) + '度，' + '最低气温' + str(today_tmp_min) + '度，' + '空气质量' + str(city_air_condition) + '，AQI' + str(city_aqi) + comfort + '，' + clothes
+weather_content = '你现在在' + location + '，最高气温' + str(today_tmp_max) + '度，' + '最低气温' + str(today_tmp_min) + '度，' + '空气质量' + str(city_air_condition) + '，AQI' + str(
+    city_aqi) + comfort + '，' + clothes + '。最高气温' + str(today_tmp_max) + '度，' + '最低气温' + str(today_tmp_min) + '度。'
 
 print(weather_content)
